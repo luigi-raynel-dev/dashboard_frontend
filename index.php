c4c38b5fe7831ca75782d6210803d1f30b510344
Luigi Raynel <devluigiraynel@gmail.com>
2020-12-24 11:14:29 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Dashboard</title>
+	<meta name="viewport" content="width=device-width, inicial-scale=1.0,maximum-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" >
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<body>
+	<div class="sidebar">
+		<div class="topo">
+			<h3>Wepp.<span style="color: #8c52ff;">Code</span></h3>
+		</div><!-- topo -->
+		<div class="menu">
+			<ul>
+				<li class="active"><i class="fa fa-clipboard-list"></i><a href=""> Home</a></li>
+				<li><i class="fa fa-copy"></i><a href=""> Conteúdo</a></li>
+				<li><i class="fa fa-chart-line"></i><a href=""> Estatísticas</a></li>
+				<li><i class="fa fa-cog"></i><a href=""> Configurações</a></li>
+				<li><i class="fa fa-question"></i><a href=""> Dúvidas</a></li>
+				<li><i class="fa fa-user"></i><a href=""> Usuários</a></li>
+				<li><i class="fa fa-sign-out-alt"></i><a href=""> Sair</a></li>
+			</ul>
+		</div><!-- menu -->
+	</div><!-- sidebar -->
+	
+	<!-- Fim da sidebar -->
+
+	<div class="main-content">
+		<header>
+			<div class="nome-usuario">
+
+				<h2><i class="fa fa-bars"></i><span>devluigiraynel@gmail.com</span><i class="fa fa-user"></i></h2>
+			</div><!-- nome-usuario -->
+			<div class="pesquisa-campo">
+<div class="icone-pesquisa"><i class="fas fa-search"></i></div><form><input type="text"></form>
+			</div><!-- pesquisa-campo -->
+		<div class="animate__animated animate__pulse animate__infinite	infinite icone-alerta btn">
+				<p>2</p>
+				<i class="fa fa-bell"></i>
+			</div><!-- icone-alerta -->
+
+			<div class="clear"></div>
+		</header>
+
+		<div class="content1">
+
+		<div class="panel">
+			<h2>Bem Vindo,<span style="color: #8c52ff;"> Luigi!</span></h2>
+			<p>Esse profissional é capaz de receber, entender e entregar um projeto completo. Atuar nesse setor significa sair da zona de conforto e aprender novas habilidades o tempo todo. Por isso, se você gosta de trabalhar em horários flexíveis, empregos criativos, engajados e com boa remuneração, a carreira em tecnologia full stack pode ser a ideal.</p>
+		</div><!-- panel -->
+
+		<div class="panel">
+			<h2>O QUE VOCÊ GOSTARIA DE FAZER?</h2>
+			<div class="container">
+				<div class="btn-flex">
+
+					<div class="btn-line">
+						<button class="btn-single">Alterar conteúdo de uma página</button>
+						<button class="btn-single">Descobrir quantas pessoas acessaram meu site</button>
+					</div><!-- btn-line -->
+
+					<div class="btn-line">
+						<button class="btn-single">Solicitar uma alteração no código</button>
+						<button class="btn-single">Permitir que outra pessoa acesse e edite o site</button>
+					</div><!-- btn-line -->
+
+					<div class="btn-line">
+						<button class="btn-single">Alterar meu cadastro</button>
+						<button class="btn-single">Escrever um artigo para o blog</button>
+						<button class="btn-single">Visualizar meu site</button>
+					</div><!-- btn-line -->
+
+				</div><!-- btn-flex -->
+			</div><!-- container -->
+		</div><!-- panel -->
+
+		<div class="panel w50">
+			<div class="container">
+				<h2>Deixe uma anotação <i class="fa fa-question" title="Faça anotações e rascunhos aqui! e mande para sua equipe!"></i></h2>
+			<form>
+				<input type="text" name="title" placeholder="Título">
+				<input type="text" name="person" placeholder="Para quem">
+				<textarea placeholder="O que você gostaria de anotar?"></textarea>
+				<input type="submit" name="enviar" value="enviar">
+			</form>
+			</div><!-- container -->
+		</div><!-- panel -->
+
+		<div class="panel w50">
+			<h2>ABRA UM TICKET. <i class="fa fa-question" title="Precisando de suporte, abra um ticket que nossa eqipue ajuda!"></i></h2>
+			<h3>Não Encontrou uma solução na Página de <a href="">Perguntas Frequentes</a>?</h3>
+			<div class="align-button">
+				<button>SOLICITE UM SUPORTE</button>
+			</div><!-- align-button -->
+		</div><!-- panel -->
+		<div class="clear"></div>
+	</div><!-- content1 -->
+
+	<div class="ultimas-atividades">
+		<div class="atividades-topo">
+			<h2>Últimas Atividades</h2>
+		</div><!-- atividades-topo -->
+		<br>
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 2 minutos</h2>
+			<p>Um usuário alterou um título de uma <a href="">página</a>!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 7 minutos</h2>
+			<p>Um usuário alterou uma imagem de uma <a href="">página</a>!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Ontem</h2>
+			<p>Uma nova <a href="">página</a> foi adicionada!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Ontem</h2>
+			<p>Um novo usuário foi adicionado!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 2 dias</h2>
+			<p>Uma nova <a href="">anotação</a> foi enivada!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 2 dias</h2>
+			<p>Uma <a href="">configuração</a> foi alterada!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 4 dias</h2>
+			<p>Uma nova <a href="">página</a> foi adicionada!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 4 dias</h2>
+			<p>Uma nova <a href="">página</a> foi adicionada!</p>
+		</div><!-- atividade-registro -->
+
+		<div class="atividade-registro">
+			<div class="circle-atividade"></div>
+			<h2>Há 6 dias</h2>
+			<p>Um usuário alterou um título de uma <a href="">página</a>!</p>
+		</div><!-- atividade-registro -->
+
+	</div><!-- ultimas-atividade -->
+	</div><!-- main-content -->
+
+	<div class="clear"></div>
+
+
+
+<!-- popup-message -->
+	<div class="bg">
+	<div class="popup">
+		<div class="closeBtn">X</div>
+		<h2>Mensagens do sistema</h2>
+		<p class="hide">Nenhuma mensagem do sistema!</p>
+		<div class="popup-message">
+		<h5>Há 2 minutos</h5>
+		<p>Um usuário alterou um título de uma <a href="">página</a></p>
+		</div>
+		<div class="popup-message">
+		<h5>Há 7 minutos</h5>
+		<p>Um usuário alterou uma imagem de uma <a href="">página</a></p>
+		</div>
+		<div>
+			<button class="remove-message">Tudo bem!</button>	
+		</div>
+		
+
+	</div><!-- popup -->
+</div><!-- bg -->
+<!-- popup-message -->
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="js/function.js"></script>
+<script src="js/message-alert.js"></script>
+<script>
+	$(function(){
+		var windowWidth = $(window)[0].innerWidth;
+		if(windowWidth  <= 768){
+			$('.nome-usuario h2').click(function(){
+			var el = $('.sidebar');
+			if(el.is(':visible')){
+				el.hide();
+				$('.main-content').css('left','0');
+			}else{
+				el.show();
+				$('.main-content').css('left','300px');
+			}
+		})
+		}
+
+		$(window).resize(function(){
+			var windowWidth = $(window)[0].innerWidth;
+		})
+	})
+</script>
+
+</body>
+</html>
